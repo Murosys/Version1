38d5d22d068b34baf882518afcd0adc0b3d94090
Nathan Tack <nathanjtack@gmail.com>
2022-10-20 11:53:18 -0400

    Adding backend

---
+++ BackEnd/app.py
@@ -0,0 +1,42 @@
+#
+# Before running this, make sure you run pip install -r requirements.txt in terminal.
+#
+from flask import Flask, request
+import flask
+import json
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route('/users', methods=["GET", "POST"])
+def users():
+    print("users endpoint reached...")
+    if request.method == "GET":
+        with open("users.json", "r") as f:
+            data = json.load(f)
+            data.append({
+                "username": "user4",
+                "pets": ["hamster"]
+            })
+            print("TEST")
+
+            return flask.jsonify(data)
+
+    if request.method == "POST":
+        print("In POST")
+        received_data = request.get_json()
+        print(f"received data: {received_data}")
+        message = received_data['data']
+        return_data = {
+            "status": "success",
+            "message": f"received: {message}"
+        }
+        return flask.Response(response=json.dumps(return_data), status=201)
+
+if __name__ == "__main__":
+    app.run("localhost", 4201)
